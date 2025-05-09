b9e0e927cc702e9e7ff1c2b31a9febde710339fd
Timothy Kim <tk-im@csu.fullerton.edu>
2025-05-08 23:48:32 -0700

    .

---
+++ src/About.jsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <p>About Me</p>
+    )
+}
+
+export default About
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { NavLink } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/about" element = {< About /> } />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
+
+function main () {
+  return (
+    
+    <img src={photo.jpg} alt="Failed to load image" />
+  )
+}
